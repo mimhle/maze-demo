d4244b3cf254af9674d2a8cee34556a5b8debabd
mimhle <40392166+mimhle@users.noreply.github.com>
2023-12-09 20:35:25 +0700

    Added more generator

--- index.js
+++ index.js
@@ -92,6 +92,8 @@ function getSolverAlg() {
             return bfsSolve;
         case "2":
             return dfsWithHeuristicSolve;
+        case "3":
+            return dfsRandomizedSolve;
         default:
             throw new Error("Invalid algorithm");
     }
@@ -220,6 +222,42 @@ function* dfsWithHeuristicSolve(maze, startX, startY, endX = maze.size.width - 1
             let h1 = heuristic(a[0], a[1], endX, endY);
             let h2 = heuristic(b[0], b[1], endX, endY);
             return h1 - h2;
+        });
+
+        for (let neighbor of connectedNeighbors) {
+            if (!maze.visited[neighbor[0]][neighbor[1]]) {
+                stack.push(neighbor);
+                maze.visited[neighbor[0]][neighbor[1]] = true;
+                maze.path.push([x, y, neighbor[0], neighbor[1]]);
+                foundUnvisitedNeighbor = true;
+                break;
+            }
+        }
+
+        if (!foundUnvisitedNeighbor) {
+            stack.pop();
+            maze.path.pop();
+        }
+
+        yield maze;
+    }
+    return maze;
+}
+
+function* dfsRandomizedSolve(maze, startX, startY, endX = maze.size.width - 1, endY = maze.size.height - 1) {
+    let stack = [];
+    stack.push([startX, startY]);
+    maze.visited[startX][startY] = true;
+
+    while (stack.length > 0) {
+        let [x, y] = stack.at(-1);
+        if (x === endX && y === endY) break;
+
+        let connectedNeighbors = maze.getConnectedNeighbors(x, y);
+        let foundUnvisitedNeighbor = false;
+
+        connectedNeighbors.sort((a, b) => {
+            return Math.random() - 0.5;
         });
 
         for (let neighbor of connectedNeighbors) {
