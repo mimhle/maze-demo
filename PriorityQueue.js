e8b06275bc137be7be485782136307adc1e1d7da
mimhle <40392166+mimhle@users.noreply.github.com>
2023-12-12 15:05:46 +0700

    Refactor

---
+++ PriorityQueue.js
@@ -0,0 +1,32 @@
+class PriorityQueue {
+    constructor() {
+        this.queue = [];
+    }
+
+    enqueue(element, priority) {
+        let obj = {element, priority};
+        let added = false;
+        for (let i = 0; i < this.queue.length; i++) {
+            if (priority < this.queue[i].priority) {
+                this.queue.splice(i, 0, obj);
+                added = true;
+                break;
+            }
+        }
+        if (!added) {
+            this.queue.push(obj);
+        }
+    }
+
+    dequeue() {
+        return this.queue.shift();
+    }
+
+    top() {
+        return this.queue[0];
+    }
+
+    isEmpty() {
+        return this.queue.length === 0;
+    }
+}
--- Solver.js
+++ Solver.js
@@ -228,39 +228,6 @@ function* bfsSolve(maze) {
     return maze;
 }
 
-class PriorityQueue {
-    constructor() {
-        this.queue = [];
-    }
-
-    enqueue(element, priority) {
-        let obj = {element, priority};
-        let added = false;
-        for (let i = 0; i < this.queue.length; i++) {
-            if (priority < this.queue[i].priority) {
-                this.queue.splice(i, 0, obj);
-                added = true;
-                break;
-            }
-        }
-        if (!added) {
-            this.queue.push(obj);
-        }
-    }
-
-    dequeue() {
-        return this.queue.shift();
-    }
-
-    top() {
-        return this.queue[0];
-    }
-
-    isEmpty() {
-        return this.queue.length === 0;
-    }
-}
-
 function* aStarEuclidean(maze) {
     const heuristic = (x1, y1, x2, y2) => {
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
